39ba36403eebb10eb90e3eaea58876facf73f5c6
tomoya <ihatov08@gmail.com>
2016-05-24 19:16:54 +0900

    コメントのシードデータの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,7 @@
 100.times do |index|
   Land.create(address: "八幡平市#{index}丁目", area: rand(100) + 100, price: rand(100) + 1000, description: "#{index}番目におすすめの物件です", image_url: "http://blog-imgs-52.fc2.com/m/a/n/manihachi/IMG_2869.jpg" )
 end
+
+300.times do |index|
+  Comment.create(body: "#{index}回目のコメントです。この物件いいね。", land_id: rand(100) + 1)
+end
