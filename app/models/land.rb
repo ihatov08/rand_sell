8133ea57f642188c4893446bb658ebcbb044f537
tomoya <ihatov08@gmail.com>
2016-05-24 20:37:17 +0900

    userとlandのアソシエーションと画像の表示

--- app/models/land.rb
+++ app/models/land.rb
@@ -1,5 +1,6 @@
 class Land < ActiveRecord::Base
   has_many :comments
+  belongs_to :user
   validates :address, presence: true, length: { maximum: 50}
   validates :area, presence: true, length: { maximum: 10}
   validates :price, presence: true, length: { maximum: 10}
