c8bb070477157bbf135a2f494f067f6d49e8e428
tomoya <ihatov08@gmail.com>
2016-08-26 09:32:28 +0900

    [modify]画像データの入れ替え

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,9 @@ User.create(first_name: "ドン", given_name: "小西", image_url: "http://next.
 User.create(first_name: "ルー", given_name: "大柴", image_url: "http://stat.ameba.jp/user_images/20151113/13/lou-oshiba/b0/64/j/t02200251_0540061613482462207.jpg", email: "rou@oshiba.com", password: "password")
 
 100.times do |index|
-  Land.create(address: "八幡平市#{index}丁目", area: rand(100) + 100, price: rand(100) + 1000, description: "#{index}番目におすすめの物件です", image_url: "http://blog-imgs-52.fc2.com/m/a/n/manihachi/IMG_2869.jpg", user_id: rand(3) + 1)
-  Land.create(address: "八幡平市#{index + 50}丁目", area: rand(100) + 100, price: rand(100) + 2000, description: "#{index}番目におすすめの物件です", image_url: "http://www.appi.co.jp/wp/wp-content/uploads/2014/04/stay_anx.jpg", user_id: rand(3) + 1)
-  Land.create(address: "八幡平市#{index + 50}丁目", area: rand(100) + 100, price: rand(100) + 2000, description: "#{index}番目におすすめの物件です", image_url: "http://stat.ameba.jp/user_images/20111229/20/chapel-hill/83/a6/j/o0448033611702150083.jpg", user_id: rand(3) + 1)
+  Land.create(address: "八幡平市#{index}丁目", area: rand(100) + 100, price: rand(100) + 1000, description: "#{index}番目におすすめの物件です", image_url: "https://lh3.googleusercontent.com/-rnmmhsi6rAQ/UOKpoFlNUXI/AAAAAAAAA2Q/mG0cinLpfrAKU4J5M6UDhCbcZtvXJ2iiQCKgB/s1024/P1020205.JPG", user_id: rand(3) + 1)
+  Land.create(address: "八幡平市#{index + 50}丁目", area: rand(100) + 100, price: rand(100) + 2000, description: "#{index}番目におすすめの物件です", image_url: "https://lh3.googleusercontent.com/-uyJq0HAplUs/UOKnVKgfdjI/AAAAAAAAAkk/vCuqjMz6TPAPtFTMFgTbzc9-lByTydoEACKgB/s1024/P1020479.JPG", user_id: rand(3) + 1)
+  Land.create(address: "八幡平市#{index + 50}丁目", area: rand(100) + 100, price: rand(100) + 2000, description: "#{index}番目におすすめの物件です", image_url: "https://lh3.googleusercontent.com/-zKCpc26EsTk/VPozbzgDfWI/AAAAAAAAM-M/i57LFukupnAAh9kynEJdFBL04yKsdmc-gCKgB/s1024/P1020662.JPG", user_id: rand(3) + 1)
 
 end
 
